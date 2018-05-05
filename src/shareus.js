f212c431d620451b9b3b2167eebe26b10641efcf
mrf3 <mrf345@gmail.com>
2018-05-05 17:27:27 +0800

    fix prevent popup

--- src/shareus.js
+++ src/shareus.js
@@ -58,7 +58,7 @@ var shareus = function ShareUs (options={}) {
             return $('<a>').click(
                 function () {
                     self.__exit__(function () {
-                        if (links[i] !== '#') window.open(links[i], '_blank').focus()
+                        if (links[i] !== '#') $('<a>').attr('href', links[i]).attr('target', '_blank').click()
                         self.options.buttonDo()
                     })
                 }
