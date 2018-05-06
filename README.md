f61687eb2f203523e4e61ab94cc219b65487a108
mrf3 <mrf345@gmail.com>
2018-05-06 18:40:15 +0800

    add one share link instead

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # shareus
 basic animated overlay for sharing on social media
+
+#### Setup :
+
+```html
+    <script type="text/javascript">
+        function shareIt (link) {
+            shareus({
+                buttonLink: link,
+                facebookLink: '' + link,
+                linkedLink: '' + link,
+                googlePLink: '' + link,
+                twitterLink: '' + link
+            }).__init__()
+        }
+    </script>
+    <button onclick="shareIt('https://github.com/mrf345/shareus')"> Download</button>
+    <button onclick="shareIt('https://github.com/mrf345/shareus')"> Download 2 </button>
+```
+
+#### Options :
+
+```JavaScript
+self.options = {
+    text: options.text || "This is the title, Share with others :", // main title text
+    textStyle: options.textStyle || {}, // text .css() style
+    overlayColor: options.overlayColor || "rgba(0,0,0,0.85)", // overlay background color
+    overlayStyle: options.overlayStyle || {}, // overlay .css() style, don't play with display:
+    buttonText: options.buttonText || "I don't care. Just download",
+    buttonClass: options.buttonClass || "", // button css classes
+    buttonStyle: options.buttonStyle || {}, // button .css() style
+    buttonLink: options.buttonLink || "#", // link to go to after button clicked
+    facebook: options.facebook || "true", // display facebook link
+    facebookLink: options.facebookLink || "#", // your facebook sharing link
+    twitter: options.twitter || "true", // display twitter link
+    twitterLink: options.twitterLink || "#", // your twitter sharing link
+    googleP: options.googleP || "true", // display google plus
+    googlePLink: options.googlePLink || "#", // your google-plus sharing link
+    linkedin: options.linkedin || "true", // display linkedin link
+    linkedinLink: options.linkedinLink || '#', // you linked sharing link
+    iconStyle: options.iconStyle || {}, // social media icon .css() style
+    effect_duration: options.effect_duration * 1000 || 1000 // overlay effect duration
+}
+```
--- src/shareus.js
+++ src/shareus.js
@@ -4,7 +4,7 @@ var shareus = function ShareUs (options={}, callback=function () {}) {
     $('.navbar').removeClass('fixed-top')
     self = {} // Object to return instead of this
     self.options = {
-        text: options.text || "Make the world organized again. For free ! Share with others :", // main title text
+        text: options.text || "This is the title, Share with others :", // main title text
         textStyle: options.textStyle || {}, // text .css() style
         overlayColor: options.overlayColor || "rgba(0,0,0,0.85)", // overlay background color
         overlayStyle: options.overlayStyle || {}, // overlay .css() style, don't play with display:
@@ -12,14 +12,15 @@ var shareus = function ShareUs (options={}, callback=function () {}) {
         buttonClass: options.buttonClass || "", // button css classes
         buttonStyle: options.buttonStyle || {}, // button .css() style
         buttonLink: options.buttonLink || "#", // link to go to after button clicked
+        shareLink: options.shareLink, // link to share on social media
         facebook: options.facebook || "true", // display facebook link
-        facebookLink: options.facebookLink || "#", // your facebook sharing link
+        facebookLink: options.facebookLink || 'https://www.facebook.com/sharer.php?u=' + shareLink || '#', // your facebook sharing link
         twitter: options.twitter || "true", // display twitter link
-        twitterLink: options.twitterLink || "#", // your twitter sharing link
+        twitterLink: options.twitterLink || 'https://twitter.com/share?url=' + shareLink || '#', // your twitter sharing link
         googleP: options.googleP || "true", // display google plus
-        googlePLink: options.googlePLink || "#", // your google-plus sharing link
+        googlePLink: options.googlePLink || 'https://plus.google.com/share?url=' + shareLink || '#', // your google-plus sharing link
         linkedin: options.linkedin || "true", // display linkedin link
-        linkedinLink: options.linkedinLink || '#', // you linked sharing link
+        linkedinLink: options.linkedinLink || 'http://www.linkedin.com/shareArticle?mini=true&amp;url=' + shareLink || '#', // you linked sharing link
         iconStyle: options.iconStyle || {}, // social media icon .css() style
         effect_duration: options.effect_duration * 1000 || 1000 // overlay effect duration
     }
